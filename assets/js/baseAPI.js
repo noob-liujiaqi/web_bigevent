8818e241c2dd98d2355815b27a8d1fbfdb790c5a
刘佳琦 <603070162@qq.com>
2020-08-03 14:16:35 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,28 @@
 //每次调用$.get()或$.post或$.ajax()的时候
 //会先调用ajaxPrefilter这个函数
 //在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options){
-    options.url='http://ajax.frontend.itheima.net'+options.
-    url
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://ajax.frontend.itheima.net' + options.
+        url
+
+    //统一为有权限的接口。设置headers请求头
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂在complete回调函数
+    options.complete = function (res) {
+        // 在complete回调函数中，可以使用res.responseJSON拿到
+        // 服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.
+            message === '身份认证失败!') {
+            //1.强制清空token
+            localStorage.removeItem('token')
+            //2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+
+    }
 })
